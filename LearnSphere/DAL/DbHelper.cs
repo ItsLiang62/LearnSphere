e89520c02b06bf2fdefbd80996f77d59cad57453
ItsLiang62 <andersonwlx62@gmail.com>
2025-12-19 11:50:36 +0800

    Updated DB structure and created DB helper class

---
+++ LearnSphere/DAL/DbHelper.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Text.RegularExpressions;
+
+namespace LearnSphere.DAL
+{
+    public class DbHelper
+    {
+        private static string connStr = ConfigurationManager.ConnectionStrings["LearnSphereDB"].ConnectionString;
+        public const string ValuePlaceholder = "[]";
+
+        public static int ExecuteNonQuery(string sql, object[] values)
+        {
+            sql = GetParameterizedSql(sql);
+            SqlParameter[] parameters = GetSqlParameters(sql, values);
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    if (parameters != null) cmd.Parameters.AddRange(parameters);
+
+                    conn.Open();
+                    return cmd.ExecuteNonQuery();
+                }
+            }
+        }
+
+        public static DataTable ExecuteQuery(string sql, object[] values) 
+        {
+            sql = GetParameterizedSql(sql);
+            SqlParameter[] parameters = GetSqlParameters(sql, values);
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand cmd = new SqlCommand(sql, conn))
+                {
+                    if (parameters != null) cmd.Parameters.AddRange(parameters);
+
+                    DataTable dt = new DataTable();
+                    new SqlDataAdapter(cmd).Fill(dt);
+                    return dt;
+                } 
+            }
+        }
+
+        private static string GetParameterizedSql(string inputSql)
+        {
+            int placeholderCount = 0;
+            int nextPlaceholderIndex = inputSql.IndexOf(ValuePlaceholder);
+            string parameterizedSql = inputSql;
+
+            while (nextPlaceholderIndex != -1)
+            {
+                placeholderCount++;
+                parameterizedSql = parameterizedSql.Remove(nextPlaceholderIndex, ValuePlaceholder.Length).Insert(nextPlaceholderIndex, "@p" + placeholderCount);
+                nextPlaceholderIndex = parameterizedSql.IndexOf(ValuePlaceholder, nextPlaceholderIndex + 1);
+            }
+
+            return parameterizedSql;
+        }
+
+        private static SqlParameter[] GetSqlParameters(string parameterizedSql, object[] values)
+        {
+            if (values is null) return null;
+
+            SqlParameter[] parameters = new SqlParameter[values.Length];
+
+            var matches = Regex.Matches(parameterizedSql, @"@\w+");
+
+            if (matches.Count != values.Length) throw new ArgumentException("Number of values does not match number of parameters");
+
+            for (int i=0; i<values.Length; i++)
+            {
+                string match = matches[i].Value;
+                parameters[i] = new SqlParameter(match, values[i] ?? DBNull.Value);
+            }
+
+            return parameters;
+        }
+    }
+}
--- Scripts/CreateTables.sql
+++ Scripts/CreateTables.sql
@@ -1,30 +1,24 @@
 USE LearnSphereDB;
 
 CREATE TABLE Administrator (
-	AdministratorID INT PRIMARY KEY,
+	AdministratorID INT IDENTITY(1, 1) PRIMARY KEY,
 	Username NVARCHAR(50) UNIQUE NOT NULL,
 	Email NVARCHAR(50) UNIQUE NOT NULL,
-	Password NVARCHAR(50) NOT NULL,
-
-	CONSTRAINT CHK_AdministratorID CHECK (AdministratorID >= 0)
+	Password NVARCHAR(50) NOT NULL
 )
 
 CREATE TABLE Learner (
-	LearnerID INT PRIMARY KEY,
+	LearnerID INT IDENTITY(1, 1) PRIMARY KEY,
 	Username NVARCHAR(50) UNIQUE NOT NULL,
 	Email NVARCHAR(50) UNIQUE NOT NULL,
-	Password NVARCHAR(50) NOT NULL,
-
-	CONSTRAINT CHK_LearnerID CHECK (LearnerID >= 0)
+	Password NVARCHAR(50) NOT NULL
 )
 
 CREATE TABLE Educator (
-	EducatorID INT PRIMARY KEY,
+	EducatorID INT IDENTITY(1, 1) PRIMARY KEY,
 	Username NVARCHAR(50) UNIQUE NOT NULL,
 	Email NVARCHAR(50) UNIQUE NOT NULL,
-	Password NVARCHAR(50) NOT NULL,
-
-	CONSTRAINT CHK_EducatorID CHECK (EducatorID >= 0)
+	Password NVARCHAR(50) NOT NULL
 )
 
 CREATE TABLE Domain (
@@ -33,7 +27,7 @@ CREATE TABLE Domain (
 )
 
 CREATE TABLE DigitalResource (
-	DigitalResourceID INT PRIMARY KEY,
+	DigitalResourceID INT IDENTITY(1, 1) PRIMARY KEY,
 	Title NVARCHAR(50) UNIQUE NOT NULL,
 	Author NVARCHAR(50),
 	PublicationYear INT,
@@ -42,25 +36,44 @@ CREATE TABLE DigitalResource (
 	Locator NVARCHAR(500) NOT NULL,
 
 	CONSTRAINT FK_DomainName_DigitalResource FOREIGN KEY (DomainName) REFERENCES Domain(DomainName),
-	CONSTRAINT CHK_DigitalResourceID CHECK (DigitalResourceID >= 0),
 	CONSTRAINT CHK_PublicationYear CHECK (PublicationYear IS NULL OR PublicationYear BETWEEN 1900 AND 2050),
 	CONSTRAINT CHK_Category CHECK (Category IN ('Book', 'Article', 'Lecture', 'Other'))
 )
 
 CREATE TABLE Forum (
-	ForumID INT PRIMARY KEY,
+	ForumID INT IDENTITY(1, 1) PRIMARY KEY,
 	Topic NVARCHAR(50) NOT NULL,
 	DomainName NVARCHAR(50) NOT NULL,
 	Tag1 NVARCHAR(50),
-	Tag2 NVARCHAR(50)
+	Tag2 NVARCHAR(50),
 
-	CONSTRAINT FK_DomainName_Forum FOREIGN KEY (DomainName) REFERENCES Domain(DomainName),
-	CONSTRAINT CHK_ForumID CHECK (ForumID >= 0)
+	CONSTRAINT FK_DomainName_Forum FOREIGN KEY (DomainName) REFERENCES Domain(DomainName)
+)
 
+CREATE TABLE LearnerPost (
+	LearnerPostID INT IDENTITY(1, 1) PRIMARY KEY,
+	ForumID INT NOT NULL,
+	LearnerID INT NOT NULL,
+	Content NVARCHAR(500) NOT NULL,
+	CreatedAt DATETIME2 NOT NULL DEFAULT GETDATE(),
+
+	CONSTRAINT FK_ForumID_LearnerPost FOREIGN KEY (ForumID) REFERENCES Forum(ForumID),
+	CONSTRAINT FK_LearnerID_LearnerPost FOREIGN KEY (LearnerID) REFERENCES Learner(LearnerID)
+)
+
+CREATE TABLE EducatorPost (
+	EducatorPostID INT IDENTITY(1, 1) PRIMARY KEY,
+	ForumID INT NOT NULL,
+	EducatorID INT NOT NULL,
+	Content NVARCHAR(500) NOT NULL,
+	CreatedAt DATETIME2 NOT NULL DEFAULT GETDATE(),
+
+	CONSTRAINT FK_ForumID_EducatorPost FOREIGN KEY (ForumID) REFERENCES Forum(ForumID),
+	CONSTRAINT FK_EducatorID_EducatorPost FOREIGN KEY (EducatorID) REFERENCES Educator(EducatorID)
 )
 
 CREATE TABLE ExamPaper (
-	ExamPaperID INT PRIMARY KEY,
+	ExamPaperID INT IDENTITY(1, 1) PRIMARY KEY,
 	Title NVARCHAR(50) NOT NULL,
 	EducatorID INT NOT NULL,
 	DomainName NVARCHAR(50) NOT NULL,
@@ -68,8 +81,7 @@ CREATE TABLE ExamPaper (
 	Tag2 NVARCHAR(50),
 
 	CONSTRAINT FK_EducatorID_ExamPaper FOREIGN KEY (EducatorID) REFERENCES Educator(EducatorID),
-	CONSTRAINT FK_DomainName_ExamPaper FOREIGN KEY (DomainName) REFERENCES Domain(DomainName),
-	CONSTRAINT CHK_ExamPaperID CHECK (ExamPaperID >= 0)
+	CONSTRAINT FK_DomainName_ExamPaper FOREIGN KEY (DomainName) REFERENCES Domain(DomainName)
 )
 
 CREATE TABLE Assessment (
