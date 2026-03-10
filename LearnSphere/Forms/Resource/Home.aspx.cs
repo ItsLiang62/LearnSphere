da242e7c9335a88741e8f1d00fc12d5d4740f95f
ItsLiang62 <andersonwlx62@gmail.com>
2026-03-11 01:09:22 +0800

    Update Home.aspx.cs

--- LearnSphere/Forms/Resource/Home.aspx.cs
+++ LearnSphere/Forms/Resource/Home.aspx.cs
@@ -84,7 +84,7 @@ namespace LearnSphere.Forms.Resource
 
         private void btnSearch_Click(object sender, EventArgs e)
         {
-            string resourcesSql = @"SELECT ID AS ResourceID, Title, Author, Category, PublicationYear AS Year
+            string resourcesSql = @"SELECT ID AS ResourceID, Title, Author, Category, PublicationYear
                                      FROM LearningResource
                                      WHERE 
                                      (@domainName = 'None' OR DomainName = @domainName)
@@ -100,7 +100,7 @@ namespace LearnSphere.Forms.Resource
                                      CASE WHEN @sortBy = 'Author (A-Z)'
                                      THEN Author END ASC,
                                      CASE WHEN @sortBy = 'Most Recent'
-                                     THEN Year END DESC,
+                                     THEN PublicationYear END DESC,
                                      CASE WHEN @sortBy = 'None' 
                                      THEN ID END ASC;";
 
@@ -117,7 +117,7 @@ namespace LearnSphere.Forms.Resource
                 {
                     { "@domainName", SelectedFilters[0] },
                     { "@category", SelectedFilters[1] },
-                    { "@sharer", sharerId },
+                    { "@sharerId", sharerId },
                     { "@sortBy", SelectedSort },
                     { "@search", search }
                 });
